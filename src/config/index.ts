6db71efd9f7f981743c5514564a1fbf85d68c32e
Gururaj Moger <gururaj9m@gmail.com>
2025-12-01 15:38:06 +0530

    Remove dotenv configuration from index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,1 @@
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export const config = {
-    port: process.env.PORT || 3000,
-    mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/jollyjet',
-    env: process.env.NODE_ENV || 'development',
-};
