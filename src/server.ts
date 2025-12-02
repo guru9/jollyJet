00e40ac4904b4caa992460e918c404edb3ad0953
guru <gururaj9m@gmail.com>
2025-12-02 17:36:36 +0530

    feat: Implement initial server setup with environment configuration and MongoDB atlas connection.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import app from "./app";
 import config from "./config";
-
+import mongoDBConnection from "./infrastructure/database/mongodb";
 
 //Start server
-const startServer = () => {
+const startServer = async () => {
     try {
+        // Connect to MongoDB first
+        await mongoDBConnection.connect();
+
         app.listen(config.port, () => {
             console.log(`🛫 jollyJet Server is running on port ${config.port}`);
         })
     } catch (error) {
         console.log('Failed to start server', error);
+        process.exit(1);
     }
 }
 
